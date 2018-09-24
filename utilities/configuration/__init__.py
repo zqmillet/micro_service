e273ec4db5403944e0ef124b28460a1368c64015
zqmillet <zqmillet@qq.com>
2018-09-24 15:46:00 +0800

    add the function execute of the class Configuration

--- utilities/configuration/__init__.py
+++ utilities/configuration/__init__.py
@@ -1,1 +1,2 @@
+from .execute import execute
 from .configuration import Configuration
--- utilities/configuration/configuration.py
+++ utilities/configuration/configuration.py
@@ -1,17 +1,24 @@
 import json
 
 from constants import FILE_MODE, ENCODE
+from utilities.configuration import execute
 
 class Configuration(dict):
-    def __init__(self, argument):
+    __prefix = ''
+
+    def __init__(self, argument, prefix = '###'):
+        self.__prefix = prefix
+
         if isinstance(argument, str):
             with open(argument, FILE_MODE.READ, encoding = ENCODE.UTF8) as file:
                 argument = json.loads(file.read())
 
         for key, value in argument.items():
             if isinstance(value, dict):
-                setattr(self, key, Configuration(value))
+                setattr(self, key, Configuration(value, prefix = self.__prefix))
             else:
+                if isinstance(value, str) and value.startswith(self.__prefix):
+                    value = execute(value.strip(self.__prefix).strip())
                 setattr(self, key, value)
             self[key] = value
 
@@ -34,6 +41,10 @@ def testcases():
 
     configuration = Configuration('./config/database.json')
     print(configuration)
+
+    configuration = Configuration('./config/math.json')
+    print(configuration)
+
 
 if __name__ == '__main__':
     testcases()
