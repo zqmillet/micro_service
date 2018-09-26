62ede7d2b18c7e2e73dc56ed8073bf2be4425e0b
张琦 <zhangqi98@huawei.com>
2018-09-26 14:22:12 +0800

    add the function iterate_lines

--- utilities/file/__init__.py
+++ utilities/file/__init__.py
@@ -1,1 +1,2 @@
 from .iterate_files import iterate_files
+from .iterate_lines import iterate_lines
---
+++ utilities/file/iterate_lines.py
@@ -0,0 +1,44 @@
+import os
+import tqdm
+
+from constants import ENCODE, FILE_MODE
+
+def get_string_size(string):
+    return len(string.encode(ENCODE.UTF8))
+
+def iterate_lines(file_path, show_progress_bar = False):
+    '''
+    this function is used to iterate lines of text file.
+    if the file does not exist, this function will not raise an error, but return nothing.
+
+    parameters:
+        - file_path:
+            this is the path of the file which will be iterated.
+
+        - show_progress_bar:
+            if show_progress_bar is True, a progress bar is shown.
+    '''
+
+    if not os.path.isfile(file_path):
+        return
+
+    file_size = os.path.getsize(file_path)
+    progress_bar = tqdm.tqdm(total = file_size) if show_progress_bar else None
+
+    with open(file_path, FILE_MODE.READ, encoding = ENCODE.UTF8) as file:
+        for line in file:
+            if show_progress_bar:
+                progress_bar.update(get_string_size(line))
+            yield line.strip('\n')
+
+def testcases():
+    for line in iterate_lines('./config/database.json', show_progress_bar = False):
+        print(line)
+
+    for line in iterate_lines('./file_which_does_not_exist'):
+        print(line)
+
+    help(iterate_lines)
+
+if __name__ == '__main__':
+    testcases()
