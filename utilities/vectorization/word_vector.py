6acb40413d2649e9774b01fe7fc37ad01708a44a
张琦 <zhangqi98@huawei.com>
2018-09-26 14:56:46 +0800

    create the module utilities.vectorization

---
+++ utilities/vectorization/word_vector.py
@@ -0,0 +1,14 @@
+import gensim
+
+class WordVector(dict):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+def testcases():
+    pass
+
+if __name__ == '__main__':
+    testcases()
