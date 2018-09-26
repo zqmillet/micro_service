8a6f6d6a578993c66ffb4884aff5d9f644983418
zqmillet <zqmillet@qq.com>
2018-09-26 23:19:30 +0800

    add the docstring for the class BaseException

--- exceptions/base.py
+++ exceptions/base.py
@@ -1,7 +1,21 @@
 import colorama
 
 class BaseException(Exception):
+    '''
+    this is the base class of all exceptions.
+
+    members:
+        - message:
+            this is the message which will be printed.
+    '''
+
     message = None
 
     def __str__(self):
-        return colorama.Fore.RED + self.__class__.__name__ + ': ' + colorama.Fore.RESET + self.message.format(**{key: value for key, value in self.__dict__.items() if not key == 'message'})
+        return colorama.Fore.RED + \
+            self.__class__.__name__ + \
+            ': ' + \
+            colorama.Fore.RESET + \
+            self.message.format(
+                **{key: value for key, value in self.__dict__.items() if not key == 'message'}
+            )
