09218e0ce1b95b855cc0a46df47b00a38d325848
张琦 <zhangqi98@huawei.com>
2018-09-27 16:44:38 +0800

    add some comments into the file utilities/scheduler/testcases.py

--- utilities/scheduler/testcases.py
+++ utilities/scheduler/testcases.py
@@ -4,19 +4,21 @@ from utilities.scheduler import Scheduler
 from constants import TRIGGER
 
 def testcases():
+    # initialize a scheduler.
     scheduler = Scheduler()
 
+    # add a task by the decorator regist_task.
     @scheduler.regist_task(trigger = TRIGGER.CRON, second = '1,2,3,4,5')
     def print_time():
         print(time.ctime())
 
-    def print_2333():
-        print(2333)
+    # add a task by the function add_task.
+    scheduler.add_task(lambda: print(time.ctime()), trigger = TRIGGER.CRON, second = '6,7,8,9,10')
 
-    scheduler.add_task(print_2333, trigger = TRIGGER.CRON, second = '6,7,8,9,10')
-
+    # start the scheduler.
     scheduler.start()
 
+    # hold on.
     while True:
         time.sleep(1)
 
