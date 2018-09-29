8bd3ba532508315c8e54995d0d3e1762a2eee45c
张琦 <zhangqi98@huawei.com>
2018-09-29 09:55:01 +0800

    modify the testcases of the module utilities.mongo

--- utilities/mongo/session.py
+++ utilities/mongo/session.py
@@ -12,24 +12,23 @@ class MongoSession:
         'authentication failed': AuthenticationError
     }
 
-    def __init__(self, host, port, username, password, timeout = 1000, maximum_retries = 10):
-        for _ in range(maximum_retries):
-            try:
-                self.__client = pymongo.MongoClient(
-                    'mongodb://{username}:{password}@{host}:{port}'.format(
-                        host     = host,
-                        port     = port,
-                        username = username,
-                        password = password
-                    ),
-                    connectTimeoutMS = timeout
-                )
-            except Exception as e:
-                for eigen, exception in self.__exception_eigen_dictionary.items():
-                    if eigen in str(e).lower():
-                        raise exception(database_type = self.__database_type, host = host, port = port)
-                else:
-                    print(e)
+    def __init__(self, host, port, username, password, timeout = 1000):
+        try:
+            self.__client = pymongo.MongoClient(
+                'mongodb://{username}:{password}@{host}:{port}'.format(
+                    host     = host,
+                    port     = port,
+                    username = username,
+                    password = password
+                ),
+                connectTimeoutMS = timeout
+            )
+        except Exception as e:
+            for eigen, exception in self.__exception_eigen_dictionary.items():
+                if eigen in str(e).lower():
+                    raise exception(database_type = self.__database_type, host = host, port = port)
+            else:
+                print(e)
 
     def fetch_database_name_list(self):
         return self.__client.database_names()
