28a5addcc86f0022b3454a4e07c4432066b8ad7b
张琦 <zhangqi98@huawei.com>
2018-09-30 14:02:38 +0800

    modify the testcases of the module utilities.mongojhhhhhhhhhhhhhhhh:w

--- utilities/argument_parser/argument_parser.py
+++ utilities/argument_parser/argument_parser.py
@@ -1,10 +1,22 @@
 import argparse
+import textwrap
+import colorama
 
 class ArgumentParser(argparse.ArgumentParser):
+    def __init__(self, *argv, **kwargs):
+        super(ArgumentParser, self).__init__(formatter_class = argparse.RawTextHelpFormatter)
+
     def add_argument(self, *argv, **kwargs):
         if 'help' in kwargs:
+            kwargs['help'] = kwargs['help'].strip().strip('.')
+            comment_list = list()
             if 'type' in kwargs:
-                kwargs['help'] += ', parameter type {type}'.format(type = kwargs['type'])
-            if 'default' in kwargs and not kwargs['default'] == '==SUPPRESS==':
-                kwargs['help'] += ', default value: {default}'.format(default = kwargs['default'])
+                comment_list.append('parameter type {type}'.format(type = kwargs['type']))
+            if 'default' in kwargs and not kwargs['default'] == argparse.SUPPRESS:
+                comment_list.append('default value: {default}'.format(default = kwargs['default']))
+
+            if len(comment_list) == 0:
+                kwargs['help'] = '\n'.join(textwrap.wrap(kwargs['help']))
+            else:
+                kwargs['help'] = '\n'.join(textwrap.wrap(kwargs['help'])) + '\n[' + colorama.import colorama.Fore.BLUE(', '.join(comment_list)) + ']'
         super(ArgumentParser, self).add_argument(*argv, **kwargs)
