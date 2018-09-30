240a723e8f4dbbc2505ce20a2fab198361a71e57
zqmillet <zqmillet@qq.com>
2018-10-01 00:22:51 +0800

    finish the docstring of the module utilities.logger

--- utilities/logger/common.py
+++ utilities/logger/common.py
@@ -7,7 +7,7 @@ def get_3rd_party_log_filter(main_title):
     this function is used to generate a logging filter, which ignore all logs which has different main_title.
 
     parameters:
-        - main_title:
+        - main_title <str>:
             this filter only remains the logs whose main title is equal to main_title.
     '''
 
@@ -21,29 +21,29 @@ def get_handler(main_title, handler, level, format, file_name):
     this function is used to generate a logging handler.
 
     parameters:
-        - main_title:
+        - main_title <str>:
             this is the main title of the logging, which will be passed into the function get_3rd_party_log_filter.
 
-        - handler:
+        - handler <logging.Handler>:
             the constructor function of the handler.
 
             for example:
                 - logging.StreamHandler
                 - logging.handlers.TimedRotatingFileHandler
 
-        - level:
+        - level <int>:
             the minimum level of the logging.
 
-        - format:
+        - format <str>:
             the type of format is str, it descripts the format of the logging.
 
             for example:
                 '[%(asctime)s][%(name)12s][%(levelname)8s][%(message)s][%(pathname)s:%(lineno)d]'
 
-        - file_name:
+        - file_name <str>:
             this is the name of the log file.
 
-    return:
+    return <logging.Handler>:
         the handler of logging.
     '''
 
--- utilities/logger/logger.py
+++ utilities/logger/logger.py
@@ -9,6 +9,10 @@ from constants import LOGGING_LEVEL, LOGGING_HANDLER, LOGGING_FORMAT
 class Logger:
     '''
     this class is used for logging.
+
+    member variables:
+        - __logger <logging.Logger>:
+            this is the logger.
     '''
 
     __logger = None
@@ -26,26 +30,26 @@ class Logger:
         this is the constructor of the class Logger.
 
         parameters:
-            - main_title:
+            - main_title <str>:
                 the main title of the logger.
 
-            - flow_type:
+            - flow_type <str>:
                 the service name of the logger.
 
-            - workspace:
+            - workspace <str>:
                 the directory of the logger file.
 
-            - format:
+            - format <str>:
                 the type of format is str, it descripts the format of the logging.
 
                 for example:
                     '[%(asctime)s][%(name)12s][%(levelname)8s][%(message)s][%(pathname)s:%(lineno)d]'
 
-            - handler_list:
+            - handler_list <list>:
                 the list of constructors of handlers.
                 the default value is [STREAM, FILE].
 
-            - level:
+            - level <int>:
                 the minimum level of the logging.
                 the default value is DEBUG.
         '''
@@ -67,16 +71,66 @@ class Logger:
             )
 
     def info(self, *args, **kwargs):
+        '''
+        provide the function info.
+
+        parameters:
+            it pass all input arguments into the function __logger.info.
+
+        return:
+            the result of __logger.info.
+        '''
+
         return self.__logger.info(*args, **kwargs)
 
     def debug(self, *args, **kwargs):
+        '''
+        provide the function debug.
+
+        parameters:
+            it pass all input arguments into the function __logger.debug.
+
+        return:
+            the result of __logger.debug.
+        '''
+
         return self.__logger.debug(*args, **kwargs)
 
     def warning(self, *args, **kwargs):
+        '''
+        provide the function warning.
+
+        parameters:
+            it pass all input arguments into the function __logger.warning.
+
+        return:
+            the result of __logger.warning.
+        '''
+
         return self.__logger.warning(*args, **kwargs)
 
     def error(self, *args, **kwargs):
+        '''
+        provide the function error.
+
+        parameters:
+            it pass all input arguments into the function __logger.error.
+
+        return:
+            the result of __logger.error.
+        '''
+
         return self.__logger.error(*args, **kwargs)
 
     def critical(self, *args, **kwargs):
+        '''
+        provide the function critical.
+
+        parameters:
+            it pass all input arguments into the function __logger.critical.
+
+        return:
+            the result of __logger.critical.
+        '''
+
         return self.__logger.critical(*args, **kwargs)
