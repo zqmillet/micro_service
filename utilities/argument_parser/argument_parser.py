4a640ec171cd9bf1659cd16ff982b1ce912e2569
张琦 <zhangqi98@huawei.com>
2018-09-30 14:13:27 +0800

    change the format of the help

--- utilities/argument_parser/argument_parser.py
+++ utilities/argument_parser/argument_parser.py
@@ -11,12 +11,12 @@ class ArgumentParser(argparse.ArgumentParser):
             kwargs['help'] = kwargs['help'].strip().strip('.')
             comment_list = list()
             if 'type' in kwargs:
-                comment_list.append('parameter type {type}'.format(type = kwargs['type']))
+                comment_list.append('# parameter type: {type}'.format(type = kwargs['type']))
             if 'default' in kwargs and not kwargs['default'] == argparse.SUPPRESS:
-                comment_list.append('default value: {default}'.format(default = kwargs['default']))
+                comment_list.append('# default value: {default}'.format(default = kwargs['default']))
 
             if len(comment_list) == 0:
                 kwargs['help'] = '\n'.join(textwrap.wrap(kwargs['help']))
             else:
-                kwargs['help'] = '\n'.join(textwrap.wrap(kwargs['help'])) + '\n[' + colorama.Fore.BLUE(', '.join(comment_list)) + ']'
+                kwargs['help'] = '\n'.join(textwrap.wrap(kwargs['help'])) + colorama.Fore.BLUE + '\n' + '\n'.join(comment_list) + colorama.Fore.RESET
         super(ArgumentParser, self).add_argument(*argv, **kwargs)
