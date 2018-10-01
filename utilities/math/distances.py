d6065cdbf63cfada69af02ec2c30f9db748851e2
zqmillet <zqmillet@qq.com>
2018-10-02 00:30:01 +0800

    add the docstring of the file utilities/math/distances.py

--- utilities/math/distances.py
+++ utilities/math/distances.py
@@ -2,6 +2,46 @@ import numpy
 import scipy.spatial
 
 def cosine_distance(matrix, vector):
+    '''
+    this function is used calculate the cosine distance'
+
+    parameters:
+        - matrix <numpy.ndarray>:
+            the list of <numpy.array>, whose dimension is same to parameter vector.
+
+            for example:
+                maxtrix = [
+                    [1, 1],
+                    [1, 0],
+                    [0, 1]
+                ]
+
+        - vector <numpy.array>/<list>:
+            the vector.
+
+            for example:
+                vector = [1, 1]
+
+    return <numpy.array>:
+        the list of cosine distance.
+
+        for example:
+            maxtrix = [
+                [1, 1],
+                [1, 0],
+                [0, 1]
+            ]
+            vector = [1, 1]
+
+            result
+            = cosine_distance(matrix, vector)
+            = [
+                1,
+                0.7071067811865476,
+                0.7071067811865476
+            ]
+    '''
+
     if isinstance(vector, list):
         vector = numpy.array(vector)
     vector = vector.reshape(1, -1)
