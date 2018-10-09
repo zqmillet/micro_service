f16c25dcdf27d2e0a90c09f6fdd347bc191f571d
zqmillet <zqmillet@qq.com>
2018-10-09 08:00:20 +0800

    modify the file utilities/vectorization/testcases.py

--- utilities/vectorization/testcases.py
+++ utilities/vectorization/testcases.py
@@ -7,7 +7,7 @@ def testcases():
 
     for word in ['中国', '公司', '测试', '编程', '苹果']:
         now = time.time()
-        print(word_vector.get_nearest_word_list(word))
+        print(word_vector.get_nearest_word_list(word, topn = 20))
         print(time.time() - now)
 
 if __name__ == '__main__':
