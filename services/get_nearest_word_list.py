eb1c12fa4c40043fb3f352e322a7bf872e385c76
zqmillet <zqmillet@qq.com>
2018-10-13 00:32:22 +0800

    add the testcases for the files services/get_nearest_word_list.py and services/get_word_vector.py

--- services/get_nearest_word_list.py
+++ services/get_nearest_word_list.py
@@ -2,5 +2,12 @@ import json
 
 from resources import word_vector
 
-def get_nearest_word_list(word, topn = 10):
+def get_nearest_word_list(word, topn = '10'):
+    topn = int(topn)
     return json.dumps(word_vector.get_nearest_word_list(word, topn), ensure_ascii = False, indent = 4)
+
+def testcases():
+    print(get_nearest_word_list('中国', topn = '3'))
+
+if __name__ == '__main__':
+    testcases()
--- services/get_word_vector.py
+++ services/get_word_vector.py
@@ -4,3 +4,9 @@ from resources import word_vector
 
 def get_word_vector(word):
     return json.dumps(word_vector[word].tolist(), indent = 4)
+
+def testcases():
+    print(get_word_vector('中国'))
+
+if __name__ == '__main__':
+    testcases()
