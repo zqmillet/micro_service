f8978423665999a199b522e023488e396d8f8c6d
zqmillet <zqmillet@qq.com>
2018-10-15 00:05:24 +0800

    add the testcases for the file utilities/webserver/server.py

--- utilities/webserver/server.py
+++ utilities/webserver/server.py
@@ -4,10 +4,51 @@ import functools
 from utilities.webserver import Application
 
 class Server(object):
+    '''
+    this class is used to manage the application.
+
+    member variables:
+        - __application <utilities.webserver.Application>:
+            the application.
+
+        - __port:
+            the port the application listens.
+    '''
+
     __application = None
     __port = None
 
     def __init__(self, configuration, logger, port):
+        '''
+        this is the constructor of the class Server.
+
+        parameters:
+            - configuration <utilities.configuration.Configuration>:
+                the configuration of the server.
+
+                for example:
+                    {
+                        "get_nearest_word_list": {
+                            "enable":   true,
+                            "api_path": "/get_nearest_word_list",
+                            "function": "### from services import get_nearest_word_list; get_nearest_word_list",
+                            "methods":  "get"
+                        },
+                        "get_word_vector": {
+                            "enable":   true,
+                            "api_path": "/get_word_vector",
+                            "function": "### from services import get_word_vector; get_word_vector",
+                            "methods":  "get/post"
+                        }
+                    }
+
+            - logger <utilities.logger.Logger>:
+                specify the logger for the server.
+
+            - port <int>:
+                specify the port for the server.
+        '''
+
         self.__application = Application(logger = logger)
         self.__port = port
 
@@ -22,12 +63,45 @@ class Server(object):
             )
 
     def start(self):
+        '''
+        this function is used to start the server.
+
+        parameters:
+            nothing.
+
+        return:
+            nothing.
+        '''
+
         self.__application.start(port = self.__port)
 
 def convert_input_argument_type(function):
+    '''
+    because, the input arguments from the requests package are all string,
+    this function is used to auto convert the input arguments of the function according to the function.__annotations__.
+
+    for example:
+        @convert_input_argument_type
+        def add(x: int, y: int) -> str:
+            return x + y
+
+        the result of the code add(x = 3, y = '2') is '5'.
+    '''
+
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
         for argument in kwargs:
             kwargs[argument] = function.__annotations__.get(argument, lambda x: x)(kwargs[argument])
         return function.__annotations__.get('return', lambda x: x)(function(*args, **kwargs))
     return wrapper
+
+def testcases():
+    @convert_input_argument_type
+    def add(x: int, y: int) -> str:
+        return x + y
+
+    print(type(add(x = 3, y = '2')))
+    print(add(x = 3, y ='2'))
+
+if __name__ == '__main__':
+    testcases()
