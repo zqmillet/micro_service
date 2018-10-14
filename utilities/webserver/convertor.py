594304aa2303c25dc077d7498516f002009c1e18
zqmillet <zqmillet@qq.com>
2018-10-14 23:46:49 +0800

    add docstring for the file utilities/webserver/convertor.py

--- utilities/webserver/convertor.py
+++ utilities/webserver/convertor.py
@@ -2,6 +2,17 @@ import json
 import numpy
 
 def convert_to_string(data):
+    '''
+    this function is used to convert the input data into the string.
+
+    parameters:
+        - data <any>:
+            the input data.
+
+    return <string>:
+        the converted data.
+    '''
+
     if isinstance(data, numpy.ndarray):
         return json.dumps(data.tolist(), ensure_ascii = False, indent = 4)
     elif isinstance(data, list):
