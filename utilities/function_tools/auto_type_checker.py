99c0683e20617092ca7a103d2dde09151f9567a3
zqmillet <zqmillet@qq.com>
2018-10-18 01:13:31 +0800

    fix the bug of the file utilities/function_tools/auto_type_convertor.py

--- utilities/function_tools/auto_type_checker.py
+++ utilities/function_tools/auto_type_checker.py
@@ -6,22 +6,20 @@ def auto_type_convertor(function):
     def wrapper(*args, **kwargs):
         parameters = inspect.signature(function).parameters
         argument_name_list = list(parameters.keys())
-        convertor_list = [parameters[argument_name].annotation for argument_name in argument_name_list]
+        checker_list = [parameters[argument_name].annotation for argument_name in argument_name_list]
         key_word_argument_name_list = argument_name_list[-len(kwargs)] if not len(kwargs) == 0 else list()
         value_list = list(args) + [kwargs[argument_name] for argument_name in key_word_argument_name_list]
 
         argument_dictionary = dict()
-        for name, value, convertor in zip(argument_name_list, value_list, convertor_list):
-            argument_dictionary[name] = convertor(value) if not convertor is inspect._empty else value
+        for name, value, checker in zip(argument_name_list, value_list, checker_list):
 
-        convertor = inspect.signature(function).return_annotation
-        result = function(**argument_dictionary)
-        return convertor(result) if not convertor is inspect._empty else result
+        result = function(*args, **kwargs)
+        return
     return wrapper
 
 def testcases():
     @auto_type_convertor
-    def add(a, b, c: int, d: int = 3):
+    def add(a, b, c: int, d: int = 3) -> str:
         return a + b + c + d
 
     @auto_type_convertor
