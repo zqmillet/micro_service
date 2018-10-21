0cf0f4c3f593e714d1b41fa59987784a0164bff9
zqmillet <zqmillet@qq.com>
2018-10-22 01:32:06 +0800

    add some docstring for the file utilities/logger/handlers.py

--- utilities/logger/handlers.py
+++ utilities/logger/handlers.py
@@ -3,25 +3,60 @@ import datetime
 import time
 import os
 
-class DailyFileHandler(logging.handlers.TimedRotatingFileHandler):
-    def __init__(self, file_name, interval = 1, backup_count = 0, extension = 'log'):
-       file_name = file_name + '_' + '0' * len(self.get_yesterday_string()) + '.' + extension
-       super(DailyFileHandler, self).__init__(
-           filename    = file_name,
-           when        = 'MIDNIGHT',
-           interval    = interval,
-           backupCount = backup_count,
-           encoding    = None,
-           delay       = False,
-           utc         = False,
-           atTime      = None
-       )
-       self.extension = extension
+from utilities.function_tools import auto_type_checker
 
-    def get_yesterday_string(self):
+class DailyFileHandler(logging.handlers.TimedRotatingFileHandler):
+    '''
+    this class inherits from the class logging.handlers.TimedRotatingFileHandler.
+
+    this class overrides functions __init__ and doRollover of the class logging.handlers.TimedRotatingFileHandler.
+    '''
+
+    @auto_type_checker
+    def __init__(self, file_name: str, interval: int = 1, backup_count: int = 0, extension: str = 'log'):
+        '''
+        this fucntion is the constructor of the class DailyFileHandler.
+
+        parameters:
+            - file_name:
+                the name of the logging file.
+
+            - interval:
+                how many days between two logging files.
+
+            - backup_count:
+                how many logging files are used to backup.
+
+            - extension:
+                the extension of the logging files.
+        '''
+
+        file_name = file_name + '_' + '0' * len(self.get_yesterday_string()) + '.' + extension
+        super(DailyFileHandler, self).__init__(
+            filename    = file_name,
+            when        = 'MIDNIGHT',
+            interval    = interval,
+            backupCount = backup_count,
+            encoding    = None,
+            delay       = False,
+            utc         = False,
+            atTime      = None
+        )
+        self.extension = extension
+
+    @auto_type_checker
+    def get_yesterday_string(self) -> str:
+        '''
+        this function is used to get the yesterday date string.
+        '''
+
         return '{0:%Y%m%d}'.format(datetime.datetime.now() - datetime.timedelta(1))
 
     def doRollover(self):
+        '''
+        this function is used to override the doRollover function of the class logging.handlers.TimedRotatingFileHandler.
+        '''
+
         if self.stream:
             self.stream.close()
             self.stream = None
