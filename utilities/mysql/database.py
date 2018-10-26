7e61c490bc4e6e16716d457198a9cef3d10ad144
zqmillet <zqmillet@qq.com>
2018-10-26 10:14:23 +0800

    add the auto_type_checker for the file utilities/mysql/database.py

--- utilities/mysql/database.py
+++ utilities/mysql/database.py
@@ -1,23 +1,25 @@
 from utilities.mysql import MySQLCollection
+from utilities.function_tools import auto_type_checker
 
 class MySQLDatabase:
     __mysql_session = None
     __database_name = None
 
-    def __init__(self, database_name, mysql_session):
+    @auto_type_checker
+    def __init__(self, database_name: str, mysql_session):
         self.__mysql_session = mysql_session
         self.__database_name = database_name
 
-    def fetch_collection_name_list(self):
+    @auto_type_checker
+    def fetch_collection_name_list(self) -> str:
         self.__mysql_session.execute('use {database_name};'.format(database_name = self.__database_name))
         return [item[0] for item in self.__mysql_session.execute('show tables')]
 
-    def fetch_collection(self, collection_name):
+    @auto_type_checker
+    def fetch_collection(self, collection_name: str) -> MySQLCollection:
         mysql_collection = MySQLCollection(collection_name = collection_name, database_name = self.__database_name, cursor = self.__mysql_session.cursor())
         return mysql_collection
 
-    def __getitem__(self, collection_name):
+    @auto_type_checker
+    def __getitem__(self, collection_name: str) -> MySQLCollection:
         return self.fetch_collection(collection_name)
-
-    def execute(self, command):
-        self.__mysql_session.execute(command)
